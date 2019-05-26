268b4c9aec9f6100ab5ff9f562fadf22a322590e
Admin <Admin@DESKTOP-89NTJEN>
2019-05-26 22:39:28 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace largestNumber
+{
+    //Program to find the largest of three numbers. 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            Console.Write("Input the 1st number :");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Input the  2nd number :");
+            num2 = int.Parse(Console.ReadLine());
+            Console.Write("Input the 3rd  number :");
+            num3 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.Write("The first Number is the greatest among three. \n\n");
+                }
+                else
+                {
+                    Console.Write("The third Number is the greatest among three. \n\n");
+                }
+            }
+            else if (num2 > num3)
+                Console.Write("The second Number is the greatest among three \n\n");
+            else
+                Console.Write("The third Number is the greatest among three \n\n");
+        
+}
+    }
+}
